5cdb25d0544929541aa661e20faac37cbdad0eac
Ronan Lana <rcpl2010@gmail.com>
2021-05-22 12:51:59 -0300

    Update stat.lua

--- src/ui/interaction/run/stat.lua
+++ src/ui/interaction/run/stat.lua
@@ -9,26 +9,15 @@
     work, under the same license. Copyright and license notices must be preserved. Contributors
     provide an express grant of patent rights.
 --]]
---[[
-    This file is part of the MapleQuestAdvisor planning tool
-    Copyleft (L) 2020 - 2021 RonanLana
-
-    GNU General Public License v3.0
-
-    Permissions of this strong copyleft license are conditioned on making available complete
-    source code of licensed works and modifications, which include larger works using a licensed
-    work, under the same license. Copyright and license notices must be preserved. Contributors
-    provide an express grant of patent rights.
---]]
 
 function on_mousemoved(x, y, dx, dy, istouch)
-    pUiStat:onmousemoved(x, y, dx, dy, istouch)
+    pUiStats:onmousemoved(x, y, dx, dy, istouch)
 end
 
 function on_mousepressed(x, y, button)
-    pUiStat:onmousepressed(x, y, button)
+    pUiStats:onmousepressed(x, y, button)
 end
 
 function on_mousereleased(x, y, button)
-    pUiStat:onmousereleased(x, y, button)
+    pUiStats:onmousereleased(x, y, button)
 end
